85e3f35851f94536592a3db77f75c5722f28285c
debbie <debbalbo@Hotmail.com>
2016-05-14 20:15:46 -0700

    got the iframe set up and to the height of window, then created a test html to have the output and iframe show the html in the output panel

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,14 @@ $(".toggleButton").click(function() {
 })
 
 
-// trying to get the textares to cover height of page, and the css isnt responding at the moemnt
-$(".panel").height($(window).height() - $("#topbar").height());
+// trying to get the textarea to cover height of page, and the css isnt responding at the moemnt--the -25 is to take away
+// any padding on the page so it will stay the entire height and not more--im still playing with those numbers
+$(".panel").height($(window).height() - $("#topbar").height() - 25);
+
+// getting the width of the iframe set up--same logic as above
+$('.panel').width($(window).width() / 2 - 15);
+
+
+// setting up iframe to display the html--thank god for Stack Overflow!
+
+$("iframe").contents().find('html').html("test html!");
