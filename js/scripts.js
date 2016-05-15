965577ff31ba49d072ae6990f71288ef87c18ce5
debbie <debbalbo@Hotmail.com>
2016-05-14 19:40:08 -0700

    added functionality to be able to select and toggle buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+// here is where I use jquery to toggle the buttons and have the active and selective ones be yellow
+
 $(".toggleButton").hover(function() {
 
-	$(this).css("background-color", "red")
-	
+	$(this).addClass('highlightedButton');
+
 }, function() {
 
-	$(this).css("background-color", "#F6451A")
+	$(this).removeClass("highlightedButton");
 });
+
+// setting up toggle and select buttons. found toggleclass in jquery docs
+
+$(".toggleButton").click(function() {
+	$(this).toggleClass("active");
+// makes sure that I can toggle the active and selected buttons
+	$(this).removeClass("highlightedButton");
+
+})
